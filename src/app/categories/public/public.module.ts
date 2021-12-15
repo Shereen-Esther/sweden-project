f9405007c18ee51f97fb2fdf82d2af4546464440
@shereen1 <shereen@surfboard.se>
2021-12-16 00:58:38 +0530

    changes to HTTp call

--- src/app/categories/public/public.module.ts
+++ src/app/categories/public/public.module.ts
@@ -3,17 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { HomeComponent } from './home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    PublicRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    BrowserModule,
-  ],
+  imports: [CommonModule, PublicRoutingModule],
 })
 export class PublicModule {}
